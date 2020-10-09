d09f5cf31f0160108544f7914ea81c9a4e0c373e
Alexander Jonas <alex.jonas@gmail.com>
2020-10-09 22:19:27 +0200

    Add dynamic navigation

--- src/js/setupNavigation.js
+++ src/js/setupNavigation.js
@@ -1,3 +1,34 @@
 import { getAllDataJs, getDataJs } from './lib'
 
-export default function setupNavigation() {}
+export default function setupNavigation() {
+  const pages = getAllDataJs('page')
+  const navLinks = getAllDataJs('nav')
+  const heading = getDataJs('heading')
+
+  navLinks.forEach(addNavLogic)
+
+  function addNavLogic(navLink) {
+    navLink.addEventListener('click', changePages)
+  }
+
+  function changePages(event) {
+    const clickedLink = event.currentTarget
+    const targetPageName = clickedLink.dataset.name
+
+    heading.textContent =
+      targetPageName === 'buddy' ? 'Code Buddys' : targetPageName
+
+    pages.forEach((page) => {
+      const pageName = page.dataset.name
+      page.classList.toggle('hidden', pageName !== targetPageName)
+    })
+
+    navLinks.forEach((navLink) => {
+      const navButton = navLink.dataset.name
+      navLink.classList.toggle(
+        'navigation__link--active',
+        navButton === targetPageName
+      )
+    })
+  }
+}
